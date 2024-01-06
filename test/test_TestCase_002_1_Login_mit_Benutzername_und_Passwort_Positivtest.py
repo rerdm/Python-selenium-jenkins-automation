5ce2173fd686769d7e9460be8d06c42d0e205777
Rene <reneerdmann87@web.de>
2024-01-06 17:53:38 +0100

    remove sys path

--- test/test_TestCase_002_1_Login_mit_Benutzername_und_Passwort_Positivtest.py
+++ test/test_TestCase_002_1_Login_mit_Benutzername_und_Passwort_Positivtest.py
@@ -2,7 +2,7 @@ import sys
 import time
 import pytest
 
-sys.path.append("C:/Users/renee/Desktop/PublicDocuments/7. Programming/Python-selenium-jenkins-automation")
+#sys.path.append("C:/Users/renee/Desktop/PublicDocuments/7. Programming/Python-selenium-jenkins-automation")
 
 from classes.InitialisiereWebdriverSetzeZoomLevel import InitialisiereWebdriverSetzeZoomLevel
 
@@ -65,6 +65,91 @@ def test_TestCase_002_1_Login_mit_Benutzername_und_Passwort_Positivtest(driver):
     aktuelles_ergebnis = driver.current_url
 
     assert aktuelles_ergebnis == bundid_test_startseite_url, f"erwartetes_ergebnis: {aktuelles_ergebnis}, aktuelles_ergebnis: {bundid_test_startseite_url}"
+    print("Vorbedingung erfolgreich")
+
+# SCHRITT 2 ###########################################################################################################
+
+    bundID_startseite.anmelde_button_klick()
+    time.sleep(waiting_time)
+
+    bundID_bn_anmleden_basis_page.benutzername_und_passwort_button_klick()
+    time.sleep(waiting_time)
+
+    bundID_bn_anmleden_basis_page.hinweis_weiter_mit_benutzername_u_passwort_popup_button_klick()
+    time.sleep(waiting_time)
+
+    # Hier wird das Erwartete mit dem aktuellen Ergebnis verglichen und reported Passed oder Failed
+
+    erwartetes_ergebnis = "https://test.id.bund.de/de/web/login/1/Benutzername"
+    aktuelles_ergebnis = driver.current_url
+
+    assert aktuelles_ergebnis == erwartetes_ergebnis, f"erwartetes_ergebnis: {aktuelles_ergebnis}, aktuelles_ergebnis: {erwartetes_ergebnis}"
+
+    bundID_bn_anmelden_anmeldeseite.benutzername_oder_email_adresse_send_keys(keys=benutzername)
+    time.sleep(waiting_time)
+
+    bundID_bn_anmelden_anmeldeseite.passwort_send_keys(keys=passwort)
+    time.sleep(waiting_time)
+
+    bundID_bn_anmelden_anmeldeseite.anmelde_button_klick()
+    time.sleep(waiting_time)
+
+    time.sleep(10)
+
+    #bundID_bn_anmelden_zugaenge_daten.zugaenge_und_daten_tab_klick()
+    #time.sleep(waiting_time)
+
+    aktuelles_ergebnis = driver.current_url
+    assert aktuelles_ergebnis == bundid_test_postfachseite, f"erwartetes_ergebnis: {aktuelles_ergebnis}, aktuelles_ergebnis: {bundid_test_postfachseite}"
+
+
+    print("Schritt 2 erfolgreich")
+
+
+
+def test_TestCase_002_1_Login_mit_Benutzername_und_Passwort_Negativtest(driver):
+
+    # Globale Variablen für die Waiting times
+
+    waiting_time = 1
+
+# TEST-DATEN INITIALISIEREN ############################################################################################
+
+    benutzername = "test07"
+    passwort = "passwort123X!"
+    bundid_test_startseite_url ="https://test.id.bund.de/de"
+    bundid_test_postfachseite = "https://test.id.bund.de/de/account/mailbox"
+
+# PAGES INITIALISIEREN  ################################################################################################
+
+    bundID_startseite = BundID_startseite(
+        driver=driver,
+        webdriver_wait=3
+    )
+
+    bundID_bn_anmleden_basis_page = BundID_bn_anmleden_basis_page(
+        driver=driver,
+        webdriver_wait=3
+    )
+
+    bundID_bn_anmelden_anmeldeseite = BundID_bn_anmelden_anmeldeseite(
+        driver=driver,
+        webdriver_wait=3
+    )
+
+    bundID_bn_anmelden_zugaenge_daten = BundID_bn_anmelden_zugaenge_daten(
+        driver=driver,
+        webdriver_wait=3
+    )
+
+# VORBEDINGUNGEN #######################################################################################################
+
+    driver.get(bundid_test_startseite_url)
+    aktuelles_ergebnis = driver.current_url
+
+    assert aktuelles_ergebnis == bundid_test_startseite_url, f"erwartetes_ergebnis: {aktuelles_ergebnis}, aktuelles_ergebnis: {bundid_test_startseite_url}"
+
+    print("Vorbedingung erfolgreich")
 
 
 # SCHRITT 2 ###########################################################################################################
@@ -101,6 +186,9 @@ def test_TestCase_002_1_Login_mit_Benutzername_und_Passwort_Positivtest(driver):
 
     aktuelles_ergebnis = driver.current_url
     assert aktuelles_ergebnis == bundid_test_postfachseite, f"erwartetes_ergebnis: {aktuelles_ergebnis}, aktuelles_ergebnis: {bundid_test_postfachseite}"
+
+    print("Schritt 2 erfolgreich")
+
 
 
 
